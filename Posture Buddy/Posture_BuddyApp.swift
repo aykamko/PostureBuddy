dbfb4953c59b1dbceb03042756b38b0783aeb377
Aleks Kamko <akamko@figma.com>
2026-04-20 16:08:54 -0700

    Initial Commit

---
+++ Posture Buddy/Posture_BuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Posture_BuddyApp.swift
+//  Posture Buddy
+//
+//  Created by Aleks Kamko on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct Posture_BuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
